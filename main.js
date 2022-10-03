a52b8fbf0b17608ec4bbd5999868502f81e95931
27Thomas27 <82878370+27Thomas27@users.noreply.github.com>
2022-10-03 18:19:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+/*functions preload, setup and draw*/ 
+/*function preload*/
+function preload() {
+    
+}
+
+/*function setup */
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+/*function draw */
+function draw() {
+
+}
+
+/*functiuon for taking snapshot */
+/*save function and give the name inside it so that it can save with that particular name. */
+function take_snapshot(){
+    save('myFilterImage.png');
+}
